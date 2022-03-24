57fbd6ccf084c5cd122ebe1050466ba7c8fb9b37
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-03-24 18:42:24 +0530

    AdminLogin

--- user-authentication/src/App.js
+++ user-authentication/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import "antd/dist/antd.css";
 import './App.css';
+import { Button } from 'antd';
+import AdminLogin from './components/AdminLogin';
 
 function App() {
   return (
     <div className="App">
-      gfdgjk
+      <AdminLogin />
     </div>
   );
 }
