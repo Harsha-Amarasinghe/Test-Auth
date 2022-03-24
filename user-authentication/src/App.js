200eb51a14250d04f58212b72933d4cab707bb74
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-03-24 19:11:44 +0530

    AdminRegistration

--- user-authentication/src/App.js
+++ user-authentication/src/App.js
@@ -3,11 +3,14 @@ import "antd/dist/antd.css";
 import './App.css';
 import { Button } from 'antd';
 import AdminLogin from './components/AdminLogin';
+import AdminRegistration from './components/AdminRegistration';
 
 function App() {
   return (
     <div className="App">
+      <AdminRegistration />
       <AdminLogin />
+      
     </div>
   );
 }
